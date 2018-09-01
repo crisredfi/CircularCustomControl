e043f6017886ce7b72186f8bcfee7fbc27d5ec02
aleix guri <crisredfi1@gmail.com>
2018-09-01 11:32:12 +0200

    Initial Commit

---
+++ customControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customControl
+//
+//  Created by Aleix Guri on 1/9/18.
+//  Copyright © 2018 Aleix Guri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customControl
+//
+//  Created by Aleix Guri on 1/9/18.
+//  Copyright © 2018 Aleix Guri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
